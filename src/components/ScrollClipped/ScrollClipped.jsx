6b0d65bf23c09dd93a3440d5fdb620c7fcb4d49a
BigWang <2972437973@qq.com>
2024-01-19 15:37:20 +0800

    to: 修复图片不显示的问题

--- src/components/ScrollClipped/ScrollClipped.jsx
+++ src/components/ScrollClipped/ScrollClipped.jsx
@@ -1,5 +1,5 @@
-import "./ScrollClipped.css"
 import { useState } from "react";
+import "./ScrollClipped.css";
 
 
 
@@ -24,9 +24,9 @@ export default function ScrollClipped() {
 
   return (
     <div>
-        <img src="../../../public/images/firstSreen.png" className="img-clipped" style={clippedStyle}></img>
+        <img src="/images/firstSreen.png" className="img-clipped" style={clippedStyle}></img>
       <h1>After my demise, lay me to rest beneath a sea of flowers.</h1>
-      <img src="../../../public/images/2-anime.jpg" className="img-normal"></img>
+      <img src="/images/2-anime.jpg" className="img-normal"></img>
     </div>
   );
 }
