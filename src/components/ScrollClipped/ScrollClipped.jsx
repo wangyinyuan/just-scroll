321143aa6adfe56a0d5a794a54bc003adb9fa5e8
BigWang <2972437973@qq.com>
2024-01-19 17:15:14 +0800

    fix: 图片不能加载

--- src/components/ScrollClipped/ScrollClipped.jsx
+++ src/components/ScrollClipped/ScrollClipped.jsx
@@ -21,12 +21,11 @@ export default function ScrollClipped() {
     transform: `scale(${1 + (scrollDistance / 600) * 0.2})`
   }
 
-
   return (
     <div>
-        <img src="/images/firstSreen.png" className="img-clipped" style={clippedStyle}></img>
+      <img src="http://s7i21oayk.sabkt.gdipper.com/just_scroll/firstSreen.png" className="img-clipped" style={clippedStyle}></img>
       <h1>After my demise, lay me to rest beneath a sea of flowers.</h1>
-      <img src="/images/2-anime.jpg" className="img-normal"></img>
+      <img src="http://s7i21oayk.sabkt.gdipper.com/just_scroll/2-anime.jpg" className="img-normal"></img>
     </div>
   );
 }
